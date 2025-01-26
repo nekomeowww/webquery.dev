25a2c8ff6cded68c0b687f423efd8ecf3961f479
Neko Ayaka <neko@ayaka.moe>
2025-01-27 07:59:41 +0800

    fix: safe area

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
-  <main class="px-4 py-4 text-center">
-    <slot />
-    <Footer />
-  </main>
+  <div
+    class="mx-auto h-full max-w-[1200px] w-full flex flex-col"
+    p="l-[max(env(safe-area-inset-left),16px)] t-[max(env(safe-area-inset-top),16px)] r-[max(env(safe-area-inset-right),16px)] b-[max(env(safe-area-inset-bottom),16px)]"
+    transition="all ease-in-out" duration-1000
+  >
+    <main w-full flex-1>
+      <slot />
+      <Footer />
+    </main>
+  </div>
 </template>
--- app/layouts/home.vue
+++ app/layouts/home.vue
@@ -1,8 +1,12 @@
 <template>
-  <main class="px-4 py-4 text-center" h-full flex flex-col>
-    <div h-full flex-1>
+  <div
+    class="mx-auto h-full max-w-[1200px] w-full flex flex-col"
+    p="l-[max(env(safe-area-inset-left),16px)] t-[max(env(safe-area-inset-top),16px)] r-[max(env(safe-area-inset-right),16px)] b-[max(env(safe-area-inset-bottom),16px)]"
+    transition="all ease-in-out" duration-1000
+  >
+    <main w-full flex-1>
       <slot />
-    </div>
-    <Footer />
-  </main>
+      <Footer />
+    </main>
+  </div>
 </template>
